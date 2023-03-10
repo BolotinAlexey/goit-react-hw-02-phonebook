6b1634896c28292bdfb61f84f34899083a5cae08
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-10 12:32:25 +0100

    done task4(filter)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   handlerChange = e => {
@@ -24,46 +30,63 @@ class App extends Component {
     });
   };
 
+  handlerChangeFilter = e =>
+    this.setState(prev => {
+      return { filter: e.target.value };
+    });
+
+  defineList = () => {
+    const subStr = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(subStr)
+    );
+  };
+
   render() {
-    const { contacts } = this.state;
+    const constVisible = this.defineList();
     return (
       <>
         <h2>Phonebook</h2>
         <form onSubmit={this.handlerSubmit}>
-          <label htmlFor="name">
-            Name
-            <input
-              id="name"
-              type="text"
-              name="name"
-              onChange={this.handlerChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-
-          <label htmlFor="tel">
-            Number
-            <input
-              id="tel"
-              type="tel"
-              name="number"
-              onChange={this.handlerChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
+          <label htmlFor="name">Name </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={this.handlerChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <br />
+          <label htmlFor="tel">Number </label>
+          <input
+            id="tel"
+            type="tel"
+            name="number"
+            onChange={this.handlerChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <br />
           <button type="submit">Add contact</button>
         </form>
 
         <h2>Contacts</h2>
+        <label htmlFor="filter">Find contacts by name:</label>
+        <input
+          id="filter"
+          type="text"
+          name="filter"
+          onChange={this.handlerChangeFilter}
+        />
+
         <ul>
-          {contacts.map(({ name, id, number }) => (
+          {constVisible.map(({ name, id, number }) => (
             <li key={id}>
               <p>
-                {name} {number}
+                {name}: {number}
               </p>
             </li>
           ))}
